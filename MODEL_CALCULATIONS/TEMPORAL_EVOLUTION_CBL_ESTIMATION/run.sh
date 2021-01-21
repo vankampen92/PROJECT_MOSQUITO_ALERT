269309f099b6dcea2dab760fce4cacfee58cc0d2
Marta <pardoaraujomarta92@gmail.com>
2021-01-21 12:41:28 +0100

    fichero .sh

---
+++ MODEL_CALCULATIONS/TEMPORAL_EVOLUTION_CBL_ESTIMATION/run.sh
@@ -0,0 +1,2 @@
+GSL_RND_SEED=98776 ./MADMODELNW -y0 0 -n 4 -v0 4 -v1 5 -v2 6 -v3 7 -sT 1.0E-04 -sN 300 -sP 6 -KK 3 -k0 50 -k1 50 -k2 50 -I0 0 -m0 0.0 -M0 10.0 -A0 0.001 -I1 1 -m1 0.0 -M1 1.0 -A1 0.001 -I2 3 -m2 0.0 -M2 10.0 -A2 0.001 -I3 4 -m3 0.0 -M3 0.1 -A3 0.001 -I4 6 -m4 0.0 -M4 10.0 -A4 0.001 -I5 7 -m5 0.0 -M5 0.1 -A5 0.001 -iP 0 -en 0 -eV 100.0 -tn 100 -t0 0.0 -t1 24.0 -t4 1 -tE 2.0 -tR 100 -xn 0 -xN 0.0 -xR 0 -DP 1 -DC 0 -D0 0 -D1 1 -D2 0 -P0 16 -a0 0 -Fn 2 -F0 Pseudo_Data_File_100P_KK3.dat -Y0 100 -F1 Time_Dependent_Downloading_Rate_25P.dat -Y1 25
+
