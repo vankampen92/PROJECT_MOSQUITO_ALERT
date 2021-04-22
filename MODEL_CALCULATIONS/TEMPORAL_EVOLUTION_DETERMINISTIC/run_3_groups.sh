a189a32fffa09e3a0b9d42165738b63512dc5b59
Marta Pardo <pardoaraujomarta92@gmail.com>
2021-04-22 11:53:26 +0200

    last changes

--- MODEL_CALCULATIONS/TEMPORAL_EVOLUTION_DETERMINISTIC/run_3_groups.sh
+++ MODEL_CALCULATIONS/TEMPORAL_EVOLUTION_DETERMINISTIC/run_3_groups.sh
@@ -1,1 +1,1 @@
- ./MADMODEL -y0 0 -n 60 -G0 10 -G1 10 -tn 2346 -t0 0.0 -t1 2377.0 -t4 1 -tR 1 -xn 0 -xN 0.0 -xR 0 -KK 2 -g0 0.098050 -k0 30 -g1 0.050714 -k1 250  -rA 100.0 -DP 1 -DC 0 -D0 0 -D1 1 -D2 0 -P0 16 -a0 0 -Fn 1 -F0 Downloads_full.dat -Y0 2346
+ ./MADMODEL -y0 0 -n 60 -G0 10 -G1 10 -tn 2346 -t0 0.0 -t1 2377.0 -t4 1 -tR 1 -xn 0 -xN 0.0 -xR 0 -KK 3 -g0 0.0773560 -k0 30 -g1 0.00132401 -k1 1175 -g0 0.0810029 -k0 1172 -rA 100.0 -DP 1 -DC 0 -D0 0 -D1 1 -D2 0 -P0 16 -a0 0 -Fn 1 -F0 Downloads_full.dat -Y0 2346
