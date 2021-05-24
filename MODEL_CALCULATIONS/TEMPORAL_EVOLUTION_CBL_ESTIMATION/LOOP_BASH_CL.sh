77161ab362f3a399d66071aae10f2d3a0eb35e48
Marta <pardoaraujomarta92@gmail.com>
2021-05-24 11:38:43 +0200

    cambios ejecución cluster

--- MODEL_CALCULATIONS/TEMPORAL_EVOLUTION_CBL_ESTIMATION/LOOP_BASH_CL.sh
+++ MODEL_CALCULATIONS/TEMPORAL_EVOLUTION_CBL_ESTIMATION/LOOP_BASH_CL.sh
@@ -4,7 +4,6 @@ export TERM=xterm
 
 /home/usuaris/m.pardo/RANDOM_SEED_FILE -N $1
 echo "A random_seed.dat file containing 30 random seeds has been saved for further use"
-
 count=0
 for (( replica=1; replica<=$1; replica++ )) 
 do 
@@ -12,7 +11,7 @@ do
 
 	count=`expr $count + 1`             # Increment arithmetically the counter
 
-	qsub -N MP.$Year -m bea -M mpardo@ceab.csic.es -v SEED=$seed,LABEL=$1,GROUP=$count,CLASS=$2  /home/usuaris/m.pardo/PROJECT_MOSQUITO_ALERT/MODEL_CALCULATIONS/TEMPORAL_EVOLUTION_CBL_ESTIMATION/my_PBS_FILE.sh
+	qsub -l -N MP.$Year -m bea -M mpardo@ceab.csic.es -v SEED=$seed,LABEL=$1,GROUP=$count,CLASS=$2  /home/usuaris/m.pardo/PROJECT_MOSQUITO_ALERT/MODEL_CALCULATIONS/TEMPORAL_EVOLUTION_CBL_ESTIMATION/my_PBS_FILE.sh
 
 	echo "Job Number: "$count "has been queued!!!"
 
